6ae507daad3098162d4df2a71e86e7d05795f463
Navid6551 <jacobwhereyagoing@gmail.com>
2024-09-23 20:30:03 +0330

    Feat (vrp-cleanup)

---
+++ resources/[vrp]/vrp-cleanup/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+client_script "@vrp-errorlog/client/cl_errorlog.lua"
+client_script "@vrp-sync/client/lib.lua"
+
+client_script 'client.lua'
+
+server_script 'server.lua'
+
+
