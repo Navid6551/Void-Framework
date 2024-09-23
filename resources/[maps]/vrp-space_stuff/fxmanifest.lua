aaac7593567dccdc760453f45e7645f0674ed2c0
Navid6551 <jacobwhereyagoing@gmail.com>
2024-09-23 14:38:36 +0330

    Feat (more maps lmao)

---
+++ resources/[maps]/vrp-space_stuff/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/space_stuff.ytyp'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+files {
+  'handling.meta',
+  'vehicles.meta',
+  'carvariations.meta',
+}
